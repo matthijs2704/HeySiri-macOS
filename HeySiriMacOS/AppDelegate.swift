1d9f7069094d5ea62c338d4f63a230c2a7be4d17
matthijs2704 <matthijs.logemann@xs4all.nl>
2016-06-16 20:27:38 +0200

    Initial Commit

---
+++ HeySiriMacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HeySiriMacOS
+//
+//  Created by Matthijs Logemann on 16/06/2016.
+//  Copyright © 2016 Matthijs Logemann. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HeySiriMacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HeySiriMacOS
+//
+//  Created by Matthijs Logemann on 16/06/2016.
+//  Copyright © 2016 Matthijs Logemann. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
